2d4e0f859c5e4b012f8194f7d646ad410886d8fa
sumin <suminon@naver.com>
2025-08-18 14:26:24 +0900

    chore: 폴더/파일 구조 정리

--- sprint4/src/lib/errors/NotFoundError.js
+++
@@ -1,8 +0,0 @@
-class NotFoundError extends Error {
-  constructor(modelName, id) {
-    super(`${modelName} (${id})을(를) 찾을 수 없습니다.`);
-    this.name = 'NotFoundError';
-  }
-}
-
-export default NotFoundError;
