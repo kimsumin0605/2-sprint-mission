e27124d5f8461af1d7605ee71ba6a6e71ce1b05e
sumin <suminon@naver.com>
2025-08-23 01:04:52 +0900

    chore: 웹 소켓 서버 연결 및 서버 진입점 파일 변경

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
-import { PORT, PUBLIC_PATH, STATIC_PATH } from './lib/constants';
 import articlesRouter from './routers/articlesRouter';
 import productsRouter from './routers/productsRouter';
 import commentsRouter from './routers/commentsRouter';
@@ -12,6 +12,7 @@ import imagesRouter from './routers/imagesRouter';
 import usersRouter from './routers/usersRouter'
 import authRouter from './routers/authRouter';
 import likesRouter from './routers/likesRouter';
+import notificationRouter from './routers/notificationRouter';
 import { defaultNotFoundHandler, globalErrorHandler } from './controllers/errorController';
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
 
-app.use(STATIC_PATH, express.static(path.resolve(process.cwd(), PUBLIC_PATH)));
+app.use('/static', express.static(path.resolve(process.cwd())));
 
 app.use('/auth', authRouter);
 app.use('/users', usersRouter);
@@ -32,10 +33,9 @@ app.use('/articles', articlesRouter);
 app.use('/products', productsRouter);
 app.use('/images', imagesRouter);
 app.use('/likes', likesRouter);
+app.use('/notifications', notificationRouter);
 
 app.use(defaultNotFoundHandler);
 app.use(globalErrorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+export default app;
