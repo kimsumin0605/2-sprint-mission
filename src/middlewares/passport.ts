c5900714e17d4dd521cf085d4d18a16cde21d0f2
sumin <suminon@naver.com>
2025-08-23 01:24:01 +0900

    refactor: 전체 코드 스타일에 맞춰 수정

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,15 +1,19 @@
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { Strategy as JwtStrategy, ExtractJwt, VerifiedCallback } from 'passport-jwt';
-import bcrypt from 'bcrypt';
-import prisma from '../lib/prismaClient';
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  VerifiedCallback,
+} from "passport-jwt";
+import bcrypt from "bcrypt";
+import prisma from "../lib/prismaClient";
 import {
   JWT_ACCESS_TOKEN_SECRET,
   JWT_REFRESH_TOKEN_SECRET,
   ACCESS_TOKEN_COOKIE_NAME,
   REFRESH_TOKEN_COOKIE_NAME,
-} from '../lib/constants';
-import { Request } from 'express';
+} from "../lib/constants";
+import { Request } from "express";
 
 // JWT payload 타입 정의
 interface JwtPayload {
@@ -18,19 +22,23 @@ interface JwtPayload {
   exp: number;
 }
 
-export const verifyAccessToken = passport.authenticate('access-token', { session: false });
+export const verifyAccessToken = passport.authenticate("access-token", {
+  session: false,
+});
 
 passport.use(
-  'local',
+  "local",
   new LocalStrategy(
-    { usernameField: 'email' },
+    { usernameField: "email" },
     async (email, password, done) => {
       try {
         const user = await prisma.user.findUnique({ where: { email } });
-        if (!user) return done(null, false, { message: '등록된 이메일이 없습니다' });
+        if (!user)
+          return done(null, false, { message: "등록된 이메일이 없습니다" });
 
         const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return done(null, false, { message: '비밀번호가 틀렸습니다.' });
+        if (!isMatch)
+          return done(null, false, { message: "비밀번호가 틀렸습니다." });
 
         return done(null, user);
       } catch (err) {
@@ -41,17 +49,19 @@ passport.use(
 );
 
 passport.use(
-  'access-token',
+  "access-token",
   new JwtStrategy(
     {
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => req?.cookies?.[ACCESS_TOKEN_COOKIE_NAME]
+        (req: Request) => req?.cookies?.[ACCESS_TOKEN_COOKIE_NAME],
       ]),
       secretOrKey: JWT_ACCESS_TOKEN_SECRET,
     },
     async (payload: JwtPayload, done: VerifiedCallback) => {
       try {
-        const user = await prisma.user.findUnique({ where: { id: payload.sub } });
+        const user = await prisma.user.findUnique({
+          where: { id: payload.sub },
+        });
         if (!user) return done(null, false);
         return done(null, user);
       } catch (err) {
@@ -62,23 +72,24 @@ passport.use(
 );
 
 passport.use(
-  'refresh-token',
+  "refresh-token",
   new JwtStrategy(
     {
       jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => req?.cookies?.[REFRESH_TOKEN_COOKIE_NAME]
+        (req: Request) => req?.cookies?.[REFRESH_TOKEN_COOKIE_NAME],
       ]),
       secretOrKey: JWT_REFRESH_TOKEN_SECRET,
       passReqToCallback: true,
     },
-    async (
-      req: Request,
-      payload: JwtPayload,
-      done: VerifiedCallback
-    ) => {
+    async (req: Request, payload: JwtPayload, done: VerifiedCallback) => {
       try {
-        const user = await prisma.user.findUnique({ where: { id: payload.sub } });
-        if (!user || user.refreshToken !== req.cookies[REFRESH_TOKEN_COOKIE_NAME]) {
+        const user = await prisma.user.findUnique({
+          where: { id: payload.sub },
+        });
+        if (
+          !user ||
+          user.refreshToken !== req.cookies[REFRESH_TOKEN_COOKIE_NAME]
+        ) {
           return done(null, false);
         }
         return done(null, user);
